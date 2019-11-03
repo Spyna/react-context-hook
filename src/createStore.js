4179a7f18e9c6e0f3d96baf0f98ce93985c296c3
Lorenzo Spinelli <lorenzo.spinelli@register.it>
2019-11-03 15:56:36 +0100

    docs: added documentation for create store

--- src/createStore.js
+++ src/createStore.js
@@ -8,8 +8,19 @@ let rawStore
  * @param {ReactElement} WrappedComponent the component to connect with the store
  * @param {Object} initialValue the initial store value or nothing
  * @param {Object} config the custom configuration. If nothing is passed will use the default config
+ * @param {Function} config.listener a function that is triggered each time the store is modified. 
+ * @param {boolean} config.proxyStore - default `true` - if true the store will be protected by a Proxy. Set to false if your environment does not support Proxy. If you use `react-context-hook` in the browser set it to true
+ * @example
+ *const initialState = { count: 10 }
  *
- * @example ../docs/createStore.examples.md
+ *const storeConfig = {
+ *  listener: state => {
+ *    console.log('state changed', state)
+ *  },
+ *  logging: true //process.env.NODE_ENV !== 'production'
+ *}
+ *
+ *export default createStore(App, initialState, storeConfig)
  */
 const createStore = (WrappedComponent, initialValue, config) => {
   return function(props) {
