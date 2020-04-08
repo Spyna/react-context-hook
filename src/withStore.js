c8fd7656d7c550c6eadc2aae9b0e5555c3bf59e3
Spyna <lscaper@gmail.com>
2020-04-08 15:09:55 +0200

    docs: update withStore docs

--- src/withStore.js
+++ src/withStore.js
@@ -7,8 +7,8 @@ import defaultConfig from './defaultConfig'
  * @param {ReactElement} WrappedComponent the component to connect with the store
  * @param {Object} initialValue the initial store value or nothing
  * @param {Object} config the custom configuration. If nothing is passed will use the default config
- * @param {Function} config.listener a function that is triggered each time the store is modified.
- * @param {boolean} config.proxyStore - default `true` - if true the store will be protected by a Proxy. Set to false if your environment does not support Proxy. If you use `react-context-hook` in the browser set it to true
+ * @param {Function} config.listener a function that is triggered each time the global state is modified.
+ * @param {boolean} config.logging - default `false` - if true it will log changes to console
  * @example
  *const initialState = { count: 10 }
  *
@@ -16,7 +16,7 @@ import defaultConfig from './defaultConfig'
  *  listener: state => {
  *    console.log('state changed', state)
  *  },
- *  logging: true //process.env.NODE_ENV !== 'production'
+ *  logging: process.env.NODE_ENV !== 'production'
  *}
  *
  *export default withStore(App, initialState, storeConfig)
